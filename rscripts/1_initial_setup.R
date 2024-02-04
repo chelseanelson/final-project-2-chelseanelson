cc217bf9ec41a04359d251e4677da74daa2fc6fb
chelseanelson <141789470+chelseanelson@users.noreply.github.com>
2024-02-04 16:04:58 -0600

    progress memo 1

--- rscripts/1_initial_setup.R
+++ rscripts/1_initial_setup.R
@@ -19,3 +19,39 @@ diabetes_data_small <- read_csv(here("data/diabetes_binary_5050split_health_indi
 # skimming the data
 diabetes_data_large %>% skimr::skim_without_charts()
 diabetes_data_small %>% skimr::skim_without_charts()
+
+# manipulating data 
+diabetes_data_large <- diabetes_data_large %>%
+  mutate(
+    diabetes_012 = factor(diabetes_012)
+  )
+
+diabetes_data_small <- diabetes_data_small %>%
+  mutate(
+    diabetes_binary = factor(diabetes_binary)
+  )
+
+# looking at target variable 
+diabetes_univariate_large <- diabetes_data_large %>% ggplot(aes(diabetes_012)) + 
+  geom_bar(stat = "count") + 
+  labs(title = "Distribution of Diabetes Diagnoses", x = "Diabetes Diagnoses",
+       y = "Count") + geom_text(stat = "count", aes(label = ..count..),
+                                vjust = -0.5) + 
+  scale_x_discrete(labels = c("no diabetes", "prediabetic", "diabetic")) + 
+  theme_minimal() 
+
+diabetes_univariate_small <- 
+  diabetes_data_small %>% ggplot(aes(diabetes_binary)) + 
+  geom_bar(stat = "count") +
+  labs(title = "Distribution of Diabetes Diagnoses", x = "Diabetes Diagnoses",
+       y = "Count") + geom_text(stat = "count", aes(label = ..count..),
+            vjust = -0.5) + 
+  scale_x_discrete(labels = c("no diabetes", "diabetic")) + 
+  theme_minimal() 
+
+# saving figures 
+ggsave(here("figures/figure-1.png"), diabetes_univariate_large)
+ggsave(here("figures/figure-2.png"), diabetes_univariate_small)
+
+
+
