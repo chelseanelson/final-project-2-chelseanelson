980234c44ea3b49795bd7ae7329424068c6ea456
chelseanelson <141789470+chelseanelson@users.noreply.github.com>
2024-02-04 16:04:36 -0600

    worked on progress memo 1

--- rscripts/1_initial_setup.R
+++ rscripts/1_initial_setup.R
@@ -10,3 +10,12 @@ library(here)
 # handle common conflicts
 tidymodels_prefer()
 
+# loading datasets
+diabetes_data_large <- read_csv(here("data/diabetes_012_health_indicators_BRFSS2015.csv")) %>% 
+  janitor::clean_names()
+diabetes_data_small <- read_csv(here("data/diabetes_binary_5050split_health_indicators_BRFSS2015.csv")) %>% 
+  janitor::clean_names()
+
+# skimming the data
+diabetes_data_large %>% skimr::skim_without_charts()
+diabetes_data_small %>% skimr::skim_without_charts()
