1b8b99d0c6eb9c6892119ea8af635d0af1ba23aa
chelseanelson <141789470+chelseanelson@users.noreply.github.com>
2024-02-04 07:33:17 -0600

    still doing the set up

---
+++ rscripts/1_initial_setup.R
@@ -0,0 +1,12 @@
+# Final Project II ----
+# Initial data checks & data splitting
+# BE AWARE: there is a random process in this script (seed set right before it)
+
+# load packages ----
+library(tidyverse)
+library(tidymodels)
+library(here)
+
+# handle common conflicts
+tidymodels_prefer()
+
