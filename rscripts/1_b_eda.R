9d7b893e5e7e4171a9c67743cea343f5da60cda7
chelseanelson <141789470+chelseanelson@users.noreply.github.com>
2024-03-12 13:21:59 -0500

    worked on figures

--- rscripts/1_b_eda.R
+++ rscripts/1_b_eda.R
@@ -77,78 +77,94 @@ univariate_discrete <- function(df, categorical_var) {
     theme_minimal()
 }
 
-cateogorical_univariate_1 <- diabetes_eda_data %>% univariate_discrete(chol_check) + 
+categorical_univariate_1 <- diabetes_eda_data %>% univariate_discrete(chol_check) + 
   scale_x_discrete(labels = c("0" = "no cholesterol check in 5 years", 
                               "1" = " yes cholesterol check in 5 years"))
 # this one has high class imbalance 
 
-cateogorical_univariate_2 <- diabetes_eda_data %>% univariate_discrete(stroke) + 
+categorical_univariate_2 <- diabetes_eda_data %>% univariate_discrete(stroke) + 
   scale_x_discrete(labels = c("0" = "never had a stroke", 
                               "1" = "yes had a stroke"))
 # high class imbalance 
 
-cateogorical_univariate_3 <- diabetes_eda_data %>% univariate_discrete(heart_diseaseor_attack) + 
+categorical_univariate_3 <- diabetes_eda_data %>% univariate_discrete(heart_diseaseor_attack) + 
   scale_x_discrete(labels = c("0" = "never had heart diseases", 
                               "1" = "yes have had heart diseases"))
 # high class imbalance 
 
-cateogorical_univariate_4 <- diabetes_eda_data %>% univariate_discrete(phys_activity)  + 
+categorical_univariate_4 <- diabetes_eda_data %>% univariate_discrete(phys_activity)  + 
   scale_x_discrete(labels = c("0" = "no physical activity in 30 days", 
                               "1" = "yes physical activity in 30 days"))
 # high class imbalance 
 
-cateogorical_univariate_5 <- diabetes_eda_data %>% univariate_discrete(fruits) + 
+categorical_univariate_5 <- diabetes_eda_data %>% univariate_discrete(fruits) + 
   scale_x_discrete(labels = c("0" = "don't consume fruit 1 or more times per day", 
                               "1" = "consumes fruit 1 or more times per day"))
 # high class imbalance 
 
-cateogorical_univariate_6 <- diabetes_eda_data %>% univariate_discrete(veggies) + 
+categorical_univariate_6 <- diabetes_eda_data %>% univariate_discrete(veggies) + 
   scale_x_discrete(labels = c("0" = "don't consume veggies 1 or more times per day", 
                               "1" = "consumes veggies 1 or more times per day"))
 # high class imbalance 
 
-cateogorical_univariate_7 <- diabetes_eda_data %>% univariate_discrete(hvy_alcohol_consump) + 
+categorical_univariate_7 <- diabetes_eda_data %>% univariate_discrete(hvy_alcohol_consump) + 
   scale_x_discrete(labels = c("0" = "not a heavy drinker", 
                               "1" = "a heavy drinker"))
   
 # high class imbalance 
 
-cateogorical_univariate_8 <- diabetes_eda_data %>% univariate_discrete(any_healthcare) + 
+categorical_univariate_8 <- diabetes_eda_data %>% univariate_discrete(any_healthcare) + 
   scale_x_discrete(labels = c("0" = "doesn't have healthcare", 
                               "1" = "has healthcare"))
   
 # high class imbalance 
 
-cateogorical_univariate_9 <- diabetes_eda_data %>% univariate_discrete(no_docbc_cost) + 
+categorical_univariate_9 <- diabetes_eda_data %>% univariate_discrete(no_docbc_cost) + 
   scale_x_discrete(labels = c("0" = "didn't need to see doctor",
                               "1" = "needed to see doctor but couldn't pay"))
 # high class imbalance 
 
-cateogorical_univariate_10 <- diabetes_eda_data %>% univariate_discrete(gen_hlth)
+categorical_univariate_10 <- diabetes_eda_data %>% univariate_discrete(gen_hlth)
 # high class imbalance but normal distrubtion 
 
-cateogorical_univariate_11 <- diabetes_eda_data %>% univariate_discrete(diff_walk) + 
+categorical_univariate_11 <- diabetes_eda_data %>% univariate_discrete(diff_walk) + 
   scale_x_discrete(labels = c("0" = "no difficultly walking up stairs", 
                               "1" = "difficultly walking up stairs"))
 # high class imbalance 
 
-cateogorical_univariate_12 <- diabetes_eda_data %>% univariate_discrete(high_bp) + 
+categorical_univariate_12 <- diabetes_eda_data %>% univariate_discrete(high_bp) + 
   scale_x_discrete(labels = c("0" = "no high BP", 
                               "1" = "high BP"))
 # low class imbalance 
 
-cateogorical_univariate_13 <- diabetes_eda_data %>% univariate_discrete(high_chol) +
+categorical_univariate_13 <- diabetes_eda_data %>% univariate_discrete(high_chol) +
   scale_x_discrete(labels = c("0" = "no high cholesterol", 
                               "1" = "high cholesterol"))
 # low class imbalance 
 
-cateogorical_univariate_14 <- diabetes_eda_data %>% univariate_discrete(smoker) +
+categorical_univariate_14 <- diabetes_eda_data %>% univariate_discrete(smoker) +
   scale_x_discrete(labels = c("0" = "had not smoked at least 100 cigarettes", 
                               "1" = "smoked at least 100 cigarettes"))
 # low class imbalance 
 
-cateogorical_univariate_15 <- diabetes_eda_data %>% univariate_discrete(sex)
+categorical_univariate_15 <- diabetes_eda_data %>% univariate_discrete(sex)
 # low class imbalance 
+
+ggsave("figures/univariate-EDA/categorical_univariate_1.png", categorical_univariate_1)
+ggsave("figures/univariate-EDA/categorical_univariate_2.png", categorical_univariate_2)
+ggsave("figures/univariate-EDA/categorical_univariate_3.png", categorical_univariate_3)
+ggsave("figures/univariate-EDA/categorical_univariate_4.png", categorical_univariate_4)
+ggsave("figures/univariate-EDA/categorical_univariate_5.png", categorical_univariate_5)
+ggsave("figures/univariate-EDA/categorical_univariate_6.png", categorical_univariate_6)
+ggsave("figures/univariate-EDA/categorical_univariate_7.png", categorical_univariate_7)
+ggsave("figures/univariate-EDA/categorical_univariate_8.png", categorical_univariate_8)
+ggsave("figures/univariate-EDA/categorical_univariate_9.png", categorical_univariate_9)
+ggsave("figures/univariate-EDA/categorical_univariate_10.png", categorical_univariate_10)
+ggsave("figures/univariate-EDA/categorical_univariate_11.png", categorical_univariate_11)
+ggsave("figures/univariate-EDA/categorical_univariate_12.png", categorical_univariate_12)
+ggsave("figures/univariate-EDA/categorical_univariate_13.png", categorical_univariate_13)
+ggsave("figures/univariate-EDA/categorical_univariate_14.png", categorical_univariate_14)
+ggsave("figures/univariate-EDA/categorical_univariate_15.png", categorical_univariate_15)
 
 # bivariate analysis ----
 
@@ -161,7 +177,7 @@ diabetes_eda_data %>%
                               "1" = "high BP")) +
   scale_fill_discrete(labels = c("0" = "no high cholesterol", 
                                  "1" = "high cholesterol")) + 
-  theme_light()
+  theme_minimal()
 
 # These are seem very correlated to each other, might be good to look at the
 # interactions between them later on 
@@ -175,7 +191,7 @@ diabetes_eda_data %>%
                               "1" = "consumes fruit 1 or more times per day")) +
   scale_x_discrete(labels = c("0" = "don't consume veggies 1 or more times per day", 
                                  "1" = "consumes veggies 1 or more times per day")) + 
-  theme_light()
+  theme_minimal()
 
 # These seem to have a somewhat strong relationship between them
 
@@ -186,7 +202,7 @@ diabetes_eda_data %>%
   ggplot(aes(bmi, high_bp)) + 
   geom_boxplot() + scale_y_discrete(labels = c("0" = "no high BP", 
                                                "1" = "high BP")) + 
-  theme_light() + labs(title = "BMI by Blood Pressure")
+  theme_minimal() + labs(title = "BMI by Blood Pressure")
 
 # no relationship here 
 
@@ -195,7 +211,7 @@ diabetes_eda_data %>%
   ggplot(aes(bmi, high_chol)) + 
   geom_boxplot() + scale_y_discrete(labels = c("0" = "no high cholesterol", 
                                                "1" = "high cholesterol")) + 
-  theme_light() + labs(title = "BMI by Cholesterol Levels")
+  theme_minimal() + labs(title = "BMI by Cholesterol Levels")
 
 # interesting that there is no relationship between high cholesterol and bmi, as well as 
 # bmi and high blood pressure, however we do see that the median for having high cholesterol or 
@@ -208,7 +224,7 @@ diabetes_eda_data %>%
   labs(title = "General Health by Cholsterol Levels") +
   scale_fill_discrete(labels = c("0" = "no high cholesterol", 
                                  "1" = "high cholesterol")) + 
-  theme_light()
+  theme_minimal()
 
 # There is a large correlation between these two variables, as general health
 # assessment gets worse, the number of people having high cholesterol increases
@@ -220,7 +236,7 @@ diabetes_eda_data %>%
   labs(title = "General Health by Cholsterol Levels") +
   scale_fill_discrete(labels = c("0" = "no high BP", 
                                  "1" = "high BP")) + 
-  theme_light()
+  theme_minimal()
 
 # There is a large correlation between these two variables, as general health
 # assessment gets worse, the number of people having high blood pressure increases
@@ -229,7 +245,7 @@ diabetes_eda_data %>%
 # BMI and General Health
 diabetes_eda_data %>%
   ggplot(aes(gen_hlth, bmi)) + 
-  geom_boxplot() + theme_light() + 
+  geom_boxplot() + theme_minimal() + 
   labs(title = "BMI by General Health")
 
 # I would not say that they are completely correlated however we do see 
@@ -245,7 +261,7 @@ diabetes_eda_data %>%
   labs(title = "Blood Pressure by Cholsterol Levels") +
   scale_fill_discrete(labels = c("0" = "no difficultly walking up stairs", 
                                  "1" = "difficultly walking up stairs")) + 
-  theme_light()
+  theme_minimal()
 
 # There seems to be an interesting relationship between these two variables
 
@@ -256,7 +272,7 @@ diabetes_eda_data %>%
   labs(title = "General Health by Physical Activity") +
   scale_fill_discrete(labels = c("0" = "no physical activity in 30 days", 
                                  "1" = "yes physical activity in 30 days")) + 
-  theme_light()
+  theme_minimal()
 
 # Not that strong of a relationship has no physical activity in 30 days 
 # seems to outweigh having physical activity in 30 days only for the poor health
@@ -272,7 +288,7 @@ diabetes_eda_data %>%
   labs(title = "Blood Pressure by Diabetes") +
   scale_x_discrete(labels = c("0" = "no high BP", 
                               "1" = "high BP")) +
-  theme_light()
+  theme_minimal()
 
 # This is pretty correlated with diabetes 
 
@@ -282,7 +298,7 @@ diabetes_eda_data %>%
   labs(title = "Cholsterol Levels by Diabetes") +
   scale_x_discrete(labels = c("0" = "no high cholsterol", 
                               "1" = "high cholsterol")) +
-  theme_light()
+  theme_minimal()
 
 # This is also pretty correlated to diabetes 
 
@@ -290,13 +306,13 @@ diabetes_eda_data %>%
   ggplot(aes(x = gen_hlth, fill = diabetes_binary)) +
   geom_bar(position = "dodge", color = "black") +
   labs(title = "General Health by Diabetes") +
-  theme_light()
+  theme_minimal()
 
 # This is also pretty correlated to diabetes 
 
 diabetes_eda_data %>%
   ggplot(aes(diabetes_binary, bmi)) + 
-  geom_boxplot() + theme_light() + 
+  geom_boxplot() + theme_minimal() + 
   labs(title = "BMI by Diabetes")
 
 # The average bmi is higher for people with diabetes although the distributions
